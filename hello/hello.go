ef8c3157fbc0bdcc9fd18db8c1ac68934eef6b56
Santiaago <santiago.ariassar@gmail.com>
2013-05-20 16:55:02 +0200

    multi declaration and initialization

--- hello/hello.go
+++ hello/hello.go
@@ -7,6 +7,24 @@ import (
 
 func main() {
 	fmt.Printf("hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
+
+	// declare var
+	var message string  = "hello go world"
+	fmt.Println(message)
+	
+	// declare multiple vars
+	var a, b, c int = 1,2,3
+	fmt.Println(message,a,b,c)
+	
+	// you don't need to specify the types:
+	var message2 = "Hello go world"
+	var d,e,f = 1, false,3
+	fmt.Println(message2,d,e,f)
+	
+	// := to have declaration and initialization at one time
+	message3 := "Hola go mundo"
+	g,h,i := 1,false,5
+	fmt.Println(message3,g,h,i)
 }
 
 
